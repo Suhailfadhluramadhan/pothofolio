693709a2352d4698bf1e37517e807b4384a32ac4
Suhailfadhluramadhan <suhail.ramadhan04@gmail.com>
2025-07-11 19:25:41 +0700

    merubah resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,12 +4,13 @@ import { Container, Row } from "react-bootstrap";
 import Particle from "../Particle";
 // import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 // import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+// Document, Page
+import {  pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  // const [width, setWidth] = useState(1200);
 
   useEffect(() => {
     setWidth(window.innerWidth);
