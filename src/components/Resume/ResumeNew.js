68b21dfaaefef02797339c2a9cf76f6849a1db71
Suhailfadhluramadhan <suhail.ramadhan04@gmail.com>
2025-07-11 19:47:45 +0700

    merubah resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Particle from "../Particle";
-import { pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, [setWidth]);
-
   return (
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}></Row>
-
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          {/* Tambahkan konten resume di sini nanti */}
         </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}></Row>
       </Container>
     </div>
   );
