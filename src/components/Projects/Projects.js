e283c3762a0dbd12bd8452e3ee2285c44baebbd0
Suhailfadhluramadhan <suhail.ramadhan04@gmail.com>
2025-07-12 16:47:51 +0700

    merubah Halaman Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import clone from "../../Assets/Projects/Spoty.png";
 import chatify from "../../Assets/Projects/Smp.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import infofilm from "../../Assets/Projects/Infofilm.png";
 
 function Projects() {
   return (
@@ -34,23 +34,23 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={infofilm}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="infofilm"
+              description="sebuah website yang berisi tentang informasi film yang di bangun dengan react.js dan menggunakan API dari OMDB. website ini juga di lengkapi dengan fitur pencarian film dan juga detail film"
+              ghLink="https://github.com/Suhailfadhluramadhan/infoFilm"
+              demoLink="https://infofilm.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={clone}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              title="Spotyfy clone"
+              description="spotyfy clone adalah sebuah website yang di bangun dengan astro.js untuk membuat tampilan yang semirip mungkin dengan spotyfy. dengan menerapkan resonsive design"
               ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              demoLink="https://spotifyproject-ten.vercel.app/"
             />
           </Col>
 
