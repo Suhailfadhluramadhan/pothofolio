d4200996f6ea4edc262dd403e8d753119a60b1ea
Suhailfadhluramadhan <suhail.ramadhan04@gmail.com>
2025-07-14 21:16:12 +0700

    merubah resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ import {
   AiOutlineUser,
 } from "react-icons/ai";
 
-import { CgFileDocument } from "react-icons/cg";
+// import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -81,7 +81,8 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+    {/*untuk cv*/}
+            {/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/resume"
@@ -89,7 +90,7 @@ function NavBar() {
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
            
 
