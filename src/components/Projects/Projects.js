d525d301239ee4f738fd124b0671b8592b9a223c
Suhailfadhluramadhan <suhail.ramadhan04@gmail.com>
2025-07-14 22:11:07 +0700

    MERUBah proyek

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
 import clone from "../../Assets/Projects/Spoty.png";
 import chatify from "../../Assets/Projects/Smp.png";
-import suicide from "../../Assets/Projects/suicide.png";
+// import suicide from "../../Assets/Projects/suicide.png";
 import infofilm from "../../Assets/Projects/Infofilm.png";
 
 function Projects() {
